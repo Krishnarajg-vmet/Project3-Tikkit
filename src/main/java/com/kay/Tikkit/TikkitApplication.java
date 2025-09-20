4f07aa3468db2676c915d5c0d21c89a2ff67a0db
Krishnaraj <krishnaraj.vmet@gmail.com>
2025-09-20 09:16:40 +0530

    Initial Project Commit for Tikkit

---
+++ src/main/java/com/kay/Tikkit/TikkitApplication.java
@@ -0,0 +1,13 @@
+package com.kay.Tikkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TikkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TikkitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kay/Tikkit/TikkitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kay.Tikkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TikkitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
